b8bc1159ba5b2970da57ff903a36514c27064ae7
Illia <mddg354@gmail.com>
2026-04-26 18:47:29 +0300

    some fix

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -10,7 +10,7 @@ export default async function NotesPage() {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ["notes", "", 1],
-    queryFn: () => fetchNotes("", 1),
+    queryFn: () => fetchNotes({ query: "", page: 1}),
   });
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
