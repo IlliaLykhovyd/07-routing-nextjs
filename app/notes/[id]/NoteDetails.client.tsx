37e64c8341db41f6a6154a05b85348338004f0c8
Illia <mddg354@gmail.com>
2026-04-30 15:54:16 +0300

    fix

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -8,13 +8,18 @@ import css from "@/components/NoteDetails/NoteDetails.module.css";
 export default function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
 
-  const { data: note } = useQuery({
-    queryKey: ["note", id], 
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
 
-  if (!note) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading, please wait...</p>;
+  if (isError || !note) return <p>Something went wrong.</p>;
 
   return (
     <div className={css.container}>
